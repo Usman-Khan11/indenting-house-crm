40a8a1a917db2d0cab7ad31ca33704c8f1bf476d
Muhammad Usman <usmanazhar357@gmail.com>
2024-11-16 13:24:49 +0500

    commit on 11-16-2024

--- resources/views/partials/sidenav.blade.php
+++ resources/views/partials/sidenav.blade.php
@@ -1,7 +1,13 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
         <a href="" class="app-brand-link">
-            <img src="{{ asset(general()->logo) }}" width="40%" />
+            <img src="{{ asset(general()->logo) }}" width="35%" />
+            {{-- <span class="app-brand-text demo menu-text fw-bold">MRI</span> --}}
+        </a>
+
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+            <i class="ti menu-toggle-icon d-none d-xl-block ti-sm align-middle"></i>
+            <i class="ti ti-x d-block d-xl-none ti-sm align-middle"></i>
         </a>
     </div>
 
@@ -14,7 +20,7 @@
         }
     @endphp
 
-    <ul class="menu-inner py-1">
+    <ul class="menu-inner">
         <li class="menu-item {{ menuActive('dashboard') }}">
             <a href="{{ route('dashboard') }}" class="menu-link">
                 <i class="menu-icon tf-icons ti ti-smart-home"></i>
